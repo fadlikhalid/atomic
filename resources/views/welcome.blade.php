ee0964b710f40c647ed83349141c89a8aca75261
Aldika Prya Bagus Ananta <68334626+pbaldika@users.noreply.github.com>
2022-10-26 10:17:33 +0800

    Update welcome page
    Removed the try again text on landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
 
 				<div class="col-md-6 col-sm-6 col-sm-pull-6">
 					<div class="mu-hero-left">
-						<h1>BookHub: leisure with literature (lets try again for the second time)</h1>
+						<h1>BookHub: leisure with literature</h1>
 						<p>BookHub is a one stop
 							literature platform for all to read. Various genres from education to mistery, from novel to magazine, we got all you want.</p>
 						<a href="" class="mu-primary-btn">Subscribe now</a>
