4249cb120998dd90484d624c6a6f558357aa2a92
pbaldika <68334626+pbaldika@users.noreply.github.com>
2022-11-01 17:03:54 +0800

    Added Book and User Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,8 +11,9 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+   public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([UserSeeder::class]);
+        $this->call([BookSeeder::class]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 'aldika2', 'shafaraananta@gmail.com', NULL, 0, NULL, NULL, '$2y$10$BJdKm9PQvaya/hxPMvueMe/LF.gnvB0.68cEO/2OVBk9EcV7lFg6K', NULL, '2022-01-19 11:00:55', '2022-01-19 11:00:55'),
+        User::create(['name' => 'aldika2', 'email' => 'shafaraananta@gmail.com', 'password' => 'password']);
+    }
+}
