e1caf3e0124b746ffaeee36e01255862918b9fe6
pbaldika <68334626+pbaldika@users.noreply.github.com>
2022-11-01 17:18:38 +0800

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,6 @@ class UserSeeder extends Seeder
     public function run()
     {
         // 'aldika2', 'shafaraananta@gmail.com', NULL, 0, NULL, NULL, '$2y$10$BJdKm9PQvaya/hxPMvueMe/LF.gnvB0.68cEO/2OVBk9EcV7lFg6K', NULL, '2022-01-19 11:00:55', '2022-01-19 11:00:55'),
-        User::create(['name' => 'aldika2', 'email' => 'shafaraananta@gmail.com', 'password' => 'password']);
+        User::create(['name' => 'john doe', 'email' => 'johnddoe123@gmail.com', 'password' => 'password']);
     }
 }
