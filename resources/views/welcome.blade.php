b8e028be42872efd90ffe0251f93cfadf5c039b5
Aldika Prya Bagus Ananta <68334626+pbaldika@users.noreply.github.com>
2022-11-02 14:37:22 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,8 @@
 
 				<div class="col-md-6 col-sm-6 col-sm-pull-6">
 					<div class="mu-hero-left">
-						<h1>BookHub: leisure with literatureTrying new atomic deployment changes</h1>
+						<h1>BookHub: leisure with literature</h1>
+                        <h2>With new books!</h2>
 						<p>BookHub is a one stop
 							literature platform for all to read. Various genres from education to mistery, from novel to magazine, we got all you want.</p>
 						<a href="" class="mu-primary-btn">Subscribe now</a>
